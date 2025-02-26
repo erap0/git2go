c12ec9d275fa40fc2a2d396761fd2dfab515a35b
erap0 <raul.palmawanto@mhs.unsoed.ac.id>
2025-02-26 09:23:48 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+26/02/2024 - Initial Commit
+26/02/2024 - Testing file status
+26/02/2024 - Testing file status pt.2
+26/02/2024 - Testing file status pt.3
+26/02/2024 - Testing file diff 
---
+++ pertama.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#include <conio.h>
+
+int A;
+int B;
+int C;
+
+void main(){
+    printf("Masukkan angka pertama : ");
+    scanf("%d", &A);
+    printf("Masukkan angka kedua : ");
+    scanf("%d", &B);
+    C = A + B;
+    printf("Hasilnya berupa angka ketiga, yakni : %d \n" , C);
+//   getch();
+};
